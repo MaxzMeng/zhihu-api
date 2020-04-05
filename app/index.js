25b3c394cbc52952e435246dda7cbd31d35689da
Xiangzhao Meng <maxzmeng@gmail.com>
2020-04-05 17:21:44 +0800

    ADD:完成图片上传

--- app/index.js
+++ app/index.js
@@ -1,8 +1,9 @@
 const Koa = require('koa');
-const bodyParser = require('koa-bodyparser');
+const koaBody = require('koa-body');
 const error = require('koa-json-error');
 const parameter = require('koa-parameter');
 const mongoose = require('mongoose');
+const path = require('path');
 const app = new Koa();
 const routing = require('./routers');
 const {connectionStr} = require('./config');
@@ -16,7 +17,14 @@ app.use(error({
         return process.env.NODE_ENV === 'production' ? rest : {stack, ...rest};
     }
 }));
-app.use(bodyParser());
+app.use(koaBody({
+    multipart: true,
+    formidable: {
+        multipart:true,
+        uploadDir: path.join(__dirname, '/public/uploads'),
+        keepExtensions: true,
+    },
+}));
 app.use(parameter(app));
 routing(app);
 
