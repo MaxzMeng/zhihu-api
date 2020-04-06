313de324e633e3641fe7fc7e4ab0026c5764ed4c
Xiangzhao Meng <maxzmeng@gmail.com>
2020-04-06 16:12:04 +0800

    ADD:用户资料补全

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -8,7 +8,9 @@ class UsersCtl {
     }
 
     async findById(ctx) {
-        const user = await User.findById(ctx.params.id);
+        const {fields = ''} = ctx.query;
+        const selectFields = fields.split(';').filter(f => f).map(f => ' +' + f).join('');
+        const user = await User.findById(ctx.params.id).select(selectFields);
         if (!user) {
             ctx.throw(404, '用户不存在');
         }
@@ -31,7 +33,7 @@ class UsersCtl {
     }
 
     async checkOwner(ctx, next) {
-        if (ctx.params.id !== ctx.state.user._id){
+        if (ctx.params.id !== ctx.state.user._id) {
             ctx.throw(403, '没有权限');
         }
         await next();
@@ -40,7 +42,14 @@ class UsersCtl {
     async update(ctx) {
         ctx.verifyParams({
             name: {type: 'string', required: false},
-            password: {type: 'string', required: false}
+            password: {type: 'string', required: false},
+            avatar_url: {type: 'string', required: false},
+            gender: {type: 'string', required: false},
+            headline: {type: 'string', required: false},
+            locations: {type: 'array', itemType: 'string', required: false},
+            business: {type: 'string', required: false},
+            employments: {type: 'array', itemType: 'object', required: false},
+            educations: {type: 'array', itemType: 'object', required: false},
         });
         const user = await User.findByIdAndUpdate(ctx.params.id, ctx.request.body);
         if (!user) {
